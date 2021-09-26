4266e2e681fb58e668276a11a7c2945f0116a500
FranciscoJavierChavez <javi_yankee@icloud.com>
2021-09-26 01:21:20 -0500

    Configuracion de rutas

---
+++ src/Contenido/01-INTRO-REACT/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div className="bg-light">
+                <div className="border bg-light pt-5 py-5">
+                    <h1 className="text-center">01 introduccion React</h1>
+                </div>
+            </div>
+            
+        );
+    }
+}
---
+++ src/Home.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Home extends Component{
+    render(){
+        return(
+            <div className="bg-light">
+                <div className="border bg-light pt-5 py-5">
+                    <h1 className="text-center">Curso React de cero a experto (Hooks y MERN)</h1>
+                    <h1 className="text-center">Autor: FERNANDO HERRERA</h1>
+                </div>
+                <div>
+                    <div className="list-group">
+                        <a href="/01intro" className="list-group-item list-group-item-action">01 intro react</a>
+                    </div>
+                </div>
+            </div>
+            
+        );
+    }
+}
