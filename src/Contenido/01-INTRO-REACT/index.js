a141cb361f0fa417618fa42b455951c36263b6e3
FranciscoJavierChavez <javi_yankee@icloud.com>
2021-09-28 09:50:30 -0500

    terminando la el la seccion 01-intro

--- src/Contenido/01-INTRO-REACT/index.js
+++ src/Contenido/01-INTRO-REACT/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 
-export default class Home extends Component{
+export default class Intro extends Component{
+    
     render(){
+        
         return(
             <div className="bg-light">
+                <div id="root1"></div>
                 <div className="border bg-light pt-5 py-5">
-                    <h1 className="text-center">01 introduccion React</h1>
+                    <h1 className="text-center">Curso React de cero a experto (Hooks y MERN)</h1>
+                    <h1 className="text-center">Autor: FERNANDO HERRERA</h1>
                 </div>
             </div>
             
         );
     }
 }
+    
